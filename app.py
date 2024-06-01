dbb6b6eabbd115ec4e0c94cc3998a3a29be037aa
angjenn <angjinwen222@gmail.com>
2024-06-01 13:41:04 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,82 @@
+from flask import Flask, render_template, request
+import google.generativeai as palm
+import replicate
+import os
+import sqlite3
+import datetime
+from flask import Markup
+
+flag = 1
+name = ""
+
+palm.configure(api_key="AIzaSyDwj5p73l7diKdHC2cLTbv5Haz5KYo4skc")
+os.environ["REPLICATE_API_TOKEN"] = "r8_ZI6j8rw4B9mXzOvSt9JRwbgWoXkuowd2ES3sn"
+
+model = {"model" : "models/chat-bison-001"}
+app = Flask(__name__)
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    return(render_template("index.html"))
+
+@app.route("/main",methods=["GET","POST"])
+def main():
+    global flag, name
+    print("flag", flag)
+    if flag == 1:
+        name = request.form.get("q")
+        current_time = datetime.datetime.now()
+        conn = sqlite3.connect("log (1).db")
+        c = conn.cursor()
+        c.execute("insert into user (name,time) values (?,?)",(name,current_time))
+        conn.commit()
+        c.close()
+        conn.close()
+        flag = 0
+    return(render_template("main.html",r=name))
+
+@app.route("/workout",methods=["GET","POST"])
+def workout():
+    return(render_template("workout.html"))
+
+@app.route("/workout_result",methods=["GET","POST"])
+def workout_result():
+    q = request.form.get("q")
+    q = "workout plan for people with age " + q + " in table form"
+    r = palm.chat(**model, messages=q)
+    return(render_template("workout_result.html",r=r.last))
+
+
+
+@app.route("/log",methods=["GET","POST"])
+def log():
+    conn = sqlite3.connect("log (1).db")
+    c = conn.cursor()
+    c.execute("select * from user")
+    r = ""
+    for row in c:
+        r += str(row) + "<br>"
+    print(r)
+    r = Markup(r)
+    c.close()
+    conn.close()
+    return(render_template("log.html",r=r))
+
+@app.route("/delete",methods=["GET","POST"])
+def delete():
+    conn = sqlite3.connect('log (1).db')
+    c = conn.cursor()
+    c.execute("delete from user")
+    conn.commit()
+    c.close()
+    conn.close()
+    return(render_template("delete.html"))
+
+@app.route("/end",methods=["GET","POST"])
+def end():
+    global flag
+    flag = 1
+    return(render_template("index.html"))
+
+if __name__ == "__main__":
+    app.run()
